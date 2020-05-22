150e1cbcba5eecf43951cec4dd13cd2d5fbed1b3
Orie Steele <orie@transmute.industries>
2020-05-22 09:25:23 -0500

    chore(ci): test

---
+++ packages/universal-wallet/src/UniversalWallet2020.test.ts
@@ -0,0 +1,8 @@
+import { UniversalWallet2020 } from '.';
+
+describe('UniversalWallet2020', () => {
+  it('contructor', () => {
+    let wallet = new UniversalWallet2020();
+    expect(wallet).toBeDefined();
+  });
+});
