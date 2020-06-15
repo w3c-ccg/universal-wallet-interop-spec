adf2da014785ecf8a95c1cc0d4b680a12dbb4dc4
Orie Steele <orie@transmute.industries>
2020-06-15 13:01:50 -0500

    save

---
+++ packages/sanity-test-cra/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Vault } from "./edv-vault";
+
+function App() {
+  return (
+    <div className="App">
+      <Vault />
+    </div>
+  );
+}
+
+export default App;
